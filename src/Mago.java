4331637118511ac9904a34e71e8c5411ad87ea90
Angel Pereira Diniz <angelpdiniz@gmail.com>
2024-03-31 19:23:25 -0300

    Novas modifica

--- src/Mago.java
+++ src/Mago.java
@@ -1,17 +1,15 @@
+
 public class Mago extends Jogador{
-    private static String nomeClasse = "Mago";
-    public Mago() {
-        super(200, 20,8);
+        private static String nomeClasse = "Mago";
+        public Mago() {
+            super(100, 20,15);
+        }
+        public Mago(String nome) {
+            super(100, 20,8, nome);
+        }
+
+        public static String getNomeClasse() {
+            return nomeClasse;
+        }
+
     }
-    public Mago(String nome) {
-        super(200, 20,8, nome);
-    }
-
-
-    public static String getNomeClasse() {
-        return nomeClasse;
-    }
-
-
-}
-
